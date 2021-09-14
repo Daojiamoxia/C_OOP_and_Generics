2be66a90c0032abd5caca970018e2bcd289e245f
Daojiamoxia <42091694+Daojiamoxia@users.noreply.github.com>
2021-09-14 17:35:03 +0800

    Update main.c

--- 顺序表的实现/main.c
+++ 顺序表的实现/main.c
@@ -1,6 +1,8 @@
 #include "SqList.h"
 
 SQLIST_TYPE_SET(int);
+SQLIST_TYPE_SET(char);
+
 
 int main(){
     SqList(int) a = InitList(int);
@@ -15,16 +17,22 @@ int main(){
     printf("is empty? %s\n", (a.ListEmpty())?"true":"false");
     printf("capacity = %d\n", a.ListCapacity());
     a.ListInsert(1,3);
+    b.ListInsert(1,'a');
     printf("is empty? %s\n", (a.ListEmpty())?"true":"false");
     printf("capacity = %d\n", a.ListCapacity());
     a.ListInsert(2,4);
+    b.ListInsert(2,'e');
     printf("capacity = %d\n", a.ListCapacity());
     a.ListInsert(3,2);
+    b.ListInsert(3,'s');
 
     for(int i = 0; i < a.ListLength(); ++i){
         printf("%d%c", a.GetElem(i + 1), (i != a.ListLength()-1)?' ':'\n');
     }
-
+    
+    for(int i = 0; i < b.ListLength(); ++i){
+        printf("%c%c", b.GetElem(i + 1), (i != b.ListLength()-1)?' ':'\n');
+    }
     for(int *it = a.begin(); it != a.end(); ++it){
         printf("%d%c", *it, (it!= a.end()-1)?' ':'\n');
     }
